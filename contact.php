7505cde82d63d2e15305d9790a61d912ad18c4a5
Phillip2410 <66375150+Phillip2410@users.noreply.github.com>
2020-06-03 17:16:33 +0200

    Added new files

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="shortcut icon" href="images/dist/icon.png">
+    <title>Kontakt - Unit-Schwarzzelt</title>
+  </head>
+
+  <body>
+
+    <div class="head2">
+      <h1 class="headline">Unit-Schwarzzelt</h1>
+      <h2 class="thema">Kontakt</h2>
+      <a class="nav" href="index.php">Home</a>
+      <a class="nav" href="info.php">Über uns</a>
+      <a class="nav" href="galerie1.php">Galerie</a>
+      <a class="nav" href="contact.php">Kontakt</a>
+    </div>
+
+  </body>
+</html>
---
+++ galerie1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/lightbox.min.css" type="text/css">
+    <script src="js/lightbox-plus-jquery.js"></script>
+
+    </script>
+    <link rel="shortcut icon" href="images/dist/icon.png">
+    <title>Gallerie - Unit-Schwarzzelt</title>
+  </head>
+
+  <body>
+
+    <div class="head2">
+      <h1 class="headline">Unit-Schwarzzelt</h1>
+      <h2 class="thema">Galerie</h2>
+      <a class="nav" href="index.php">Home</a>
+      <a class="nav" href="info.php">Über uns</a>
+      <a class="nav" href="galerie1.php">Galerie</a>
+      <a class="nav" href="contact.php">Kontakt</a>
+    </div>
+
+      <div class="gallery">     <!-- Kleine Bilder haben eine Breite von 360 pixeln -->
+        <a href="images/pics/bild1.png" data-lightbox="mygallery"><img src="images/pics/bild1-klein.png"></a>
+        <a href="images/pics/bild2.png" data-lightbox="mygallery"><img src="images/pics/bild2-klein.png"></a>
+        <a href="images/pics/bild3.png" data-lightbox="mygallery"><img src="images/pics/bild3-klein.png"></a>
+        <a href="images/pics/bild4.png" data-lightbox="mygallery"><img src="images/pics/bild4-klein.png"></a>
+        <a href="images/pics/bild5.png" data-lightbox="mygallery"><img src="images/pics/bild5-klein.png"></a>
+        <a href="images/pics/bild6.png" data-lightbox="mygallery"><img src="images/pics/bild6-klein.png"></a>
+        <a href="images/pics/bild7.png" data-lightbox="mygallery"><img src="images/pics/bild7-klein.png"></a>
+        <a href="images/pics/bild8.png" data-lightbox="mygallery"><img src="images/pics/bild8-klein.png"></a>
+        <a href="images/pics/bild9.png" data-lightbox="mygallery"><img src="images/pics/bild9-klein.png"></a>
+        <a href="images/pics/bild10.png" data-lightbox="mygallery"><img src="images/pics/bild10-klein.png"></a>
+      </div>
+
+  </body>
+</html>
